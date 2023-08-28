88ab7eef9727bad6c51e0877176360d408116873
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-08-29 03:25:13 +0400

    Practice: Catch'em All Commit N2

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -8,11 +8,11 @@ public class CatchEmAll {
     //You may set another exception in this field;
     static Exception exception = new FileNotFoundException();
 
-    public static void riskyMethod() throws Exception {
+    public static void riskyMethod() throws Exception, FileNotFoundException, IOException {
         throw exception;
     }
 
-    public static void main(String[] args) throws Exception, FileNotFoundException, IOException {
+    public static void main(String[] args) throws Exception {
         try {
             riskyMethod();
         } catch (FileNotFoundException e) {
