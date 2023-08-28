20f2d3ec069aee2607a321fdd64c02dafaa24555
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-08-29 03:18:03 +0400

    Practice: Catch'em All Commit N1

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,6 +1,7 @@
 package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CatchEmAll {
 
@@ -12,6 +13,14 @@ public class CatchEmAll {
     }
 
     public static void main(String[] args) throws Exception {
-        riskyMethod();
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            System.out.println("Resource is missing");
+        } catch (IOException e) {
+            System.out.println("Resource error");
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.out.println(e);
+        }
     }
 }
